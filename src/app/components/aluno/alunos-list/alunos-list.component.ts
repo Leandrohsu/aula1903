3e9030646bc66fc5277269909abd1443425778ee
Leandro So Fun Hsu Liu <leandrohsu@outlook.com>
2025-03-31 12:04:45 -0300

    atualizacao

--- src/app/components/aluno/alunos-list/alunos-list.component.ts
+++ src/app/components/aluno/alunos-list/alunos-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { Aluno } from '../../../models/aluno';
 import { AlunoService } from '../../../services/aluno.service';
+import Swal from 'sweetalert2';
+import { MdbModalModule, MdbModalRef, MdbModalService } from '../alunos-form/alunos-form.component';
 
 @Component({
   selector: 'app-alunos-list',
@@ -12,40 +14,71 @@ import { AlunoService } from '../../../services/aluno.service';
 export class AlunosListComponent {
 
   lista: Aluno[] = [];
-  
-  alunoService = inject(AlunoService);
+  pesquisa: string = "";
+  alunoEdit!: Aluno;
 
-  constructor() {
+  @ViewChild("modalAlunoForm") modalAlunoForm!: TemplateRef<any>; //referência ao template da modal
+  modalService = inject(MdbModalService); //para abrir a modal
+  modalRef!: MdbModalRef<any>; //vc conseguir fechar a modal depois
+
+
+  alunoService = inject(AlunoService);
+  constructor(){
     this.findAll();
   }
 
 
   findAll(){
-   
+
     this.alunoService.findAll().subscribe({
       next: (listaRetornada) => {
         this.lista = listaRetornada;
       },
       error: (erro) => {
-        alert('Deu erro!');
+        Swal.fire(erro.error, '', 'error');
       }
     });
   
   }
 
   delete(aluno: Aluno){
-    if(confirm('Deseja deletar isso aí?')){
 
-      this.alunoService.deleteById(aluno.id).subscribe({
-        next: (mensagem) => {
-          alert(mensagem);
-          this.findAll();
-        },
-        error: (erro) => {
-          alert('Deu erro!');
-        }
-      });
+    Swal.fire({
+      title: 'Deseja mesmo deletar?',
+      showCancelButton: true,
+      confirmButtonText: 'Sim',
+      cancelButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
 
-    }
+        this.alunoService.deleteById(aluno.id).subscribe({
+          next: (mensagem) => {
+            Swal.fire(mensagem, '', 'success');
+            this.findAll();
+          },
+          error: (erro) => {
+            Swal.fire(erro.error, '', 'error');
+          }
+        });
+        
+      }
+    });
+      
+
+  }
+
+  new(){ //ABRIR
+    this.alunoEdit = new Aluno(); 
+    this.modalRef = this.modalService.open(this.modalAlunoForm, { modalClass: 'modal-xl'});
+  }
+
+  edit(aluno: Aluno){
+    this.alunoEdit = aluno;
+    this.modalRef = this.modalService.open(this.modalAlunoForm, { modalClass: 'modal-xl'});
+  }
+
+  meuEventoTratamento(mensagem:any){
+    this.findAll();
+    this.modalRef.close();
   }
 }
